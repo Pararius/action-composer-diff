fcd241076cd0d4301cc432b1dd1e8514c61c48c2
pkruithof <pkruithof@gmail.com>
2021-01-28 17:58:07 +0100

    Post comment

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,8 @@
 FROM composer:2
+RUN apk add --no-cache jq curl
 
 ENV COMPOSER_BIN_DIR=/usr/local/bin
 RUN composer require davidrjonas/composer-lock-diff
-
-WORKDIR /github/workspace
 
 COPY entrypoint.sh /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,24 @@
-#!/bin/sh -l
+#!/bin/bash -l
 
-diff="$(composer-lock-diff --from=HEAD --md)"
-diff="${diff//'%'/'%25'}"
-diff="${diff//$'\n'/'%0A'}"
-diff="${diff//$'\r'/'%0D'}"
+REPO="$1"
+PR="$2"
+FROM="$3"
+TO="$4"
+TOKEN="$5"
 
-echo "::set-output name=diff::$diff"
+DIFF="$(composer-lock-diff --from="$FROM" --to="$TO" --md)"
+BODY="$(printf 'The following Composer dependencies have been changed:\n\n%s' "${DIFF}" | jq -aRs '.')"
+
+curl -q \
+  -X POST \
+  -H "Authorization: token ${TOKEN}" \
+  -H "Accept: application/vnd.github.v3+json" \
+  "https://api.github.com/repos/${REPO}/issues/${PR}/comments" \
+  -d "{\"body\":${BODY}}"
+
+outputDiff="${DIFF}"
+outputDiff="${outputDiff//'%'/'%25'}"
+outputDiff="${outputDiff//$'\n'/'%0A'}"
+outputDiff="${outputDiff//$'\r'/'%0D'}"
+
+echo "::set-output name=diff::$outputDiff"
